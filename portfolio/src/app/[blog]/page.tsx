457ee5f07c6a1506d37983ea0f60f0806cf85931
Asad Raza <143411340+asadrazanaqv@users.noreply.github.com>
2024-02-02 06:01:50 +0500

    upd

---
+++ portfolio/src/app/[blog]/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useRouter } from "next/router"
+
+
+const Post = () => {
+    const router = useRouter()
+    const { slug } = router.query
+    
+    return(
+        <div>
+            <h1>Post: {slug}</h1>
+        </div>
+    )
+}
